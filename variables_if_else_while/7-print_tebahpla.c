332cb50a41eaa5d09e85529a12a79df3da61d6df
saad <10704@holbertonstudents.com>
2025-02-15 16:36:40 +0000

    7-print_tebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase in reverse order,
+ *              followed by a new line, using only putchar.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  char letter;
+
+  for (letter = 'z'; letter >= 'a'; letter--)
+    {
+      putchar(letter);
+    }
+
+  putchar('\n');
+
+  return (0);
+}
