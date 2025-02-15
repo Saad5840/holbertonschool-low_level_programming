b6b450df7ff7778c8dd91ab721c368bdff328403
saad <10704@holbertonstudents.com>
2025-02-15 15:42:00 +0000

    2-print_alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
+	char letter;
 
-  char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-  for (letter='a'; letter<='z'; letter++)
-    putchar(letter);
-  putchar('\n');
+	putchar('\n');
 
-  return (0);
+	return (0);
 }
