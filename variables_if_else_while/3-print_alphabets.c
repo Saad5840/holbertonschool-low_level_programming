c805c26cc4cb0eaaa11378f6b0af5309a0692661
saad <10704@holbertonstudents.com>
2025-02-15 15:53:31 +0000

    3-print_alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,15 +9,18 @@
  */
 int main(void)
 {
-    char letter;
+	char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-        putchar(letter);
+	/* Print lowercase letters */
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-    for (letter = 'A'; letter <= 'Z'; letter++)
-        putchar(letter);
+	/* Print uppercase letters */
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
 
-    putchar('\n');
+	/* Print a new line */
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
