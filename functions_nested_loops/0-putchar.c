f2de95a266feebff6d79bd7ef643674a82fa1f03
Saad <10704@holbertonstudents.com>
2025-02-16 15:29:57 +0000

    0-putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ *
+ * Main - Entry point
+ *
+ * Discription: Print _putchar
+ *
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+  printf("_putchar\n");
+
+
+  return (0);
+}
