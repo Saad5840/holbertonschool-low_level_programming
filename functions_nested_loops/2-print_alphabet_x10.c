29b1430d22ca617530655cedd7ff1ee0b339cdfa
Saad <10704@holbertonstudents.com>
2025-02-17 16:17:56 +0000

    2-alphabet_x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints the alphabet 10 times, each followed by a new line
+ * print_alphabet_x10 - prints the alphabet 10 times, followed by a new line
  *
  * This function loops 10 times, printing the alphabet in lowercase each time,
  * followed by a new line.
