9a7d280f1a9609f17a150e3ce74441fb6275d66f
Saad <10704@holbertonstudents.com>
2025-02-24 16:58:37 +0000

    6-lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal.
+ * @n: The number of underscores to print.
+ */
+
+void print_line(int n)
+{
+int i;
+
+if (n > 0)
+{
+	for (i = 0; i < n; i++)
+	{
+		_putchar('-'); 
+	}
+
+}
+
+_putchar('\n');
+
+}
