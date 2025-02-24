e2d3df3d520c644a9ce9939273b3cc27d4eb6212
Saad <10704@holbertonstudents.com>
2025-02-24 17:03:17 +0000

    6-lines

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -4,21 +4,16 @@
  * print_line - Draws a straight line in the terminal.
  * @n: The number of underscores to print.
  */
-
 void print_line(int n)
 {
-int i;
+    int i;
 
-if (n > 0)
-{
-	for (i = 0; i < n; i++)
-	{
-		_putchar('-'); 
-	}
-	_putchar('\n');
-
-}
-
-_putchar('\n');
-
+    if (n > 0)
+    {
+        for (i = 0; i < n; i++)
+        {
+            _putchar('_');  
+        }
+    }
+    _putchar('\n');  
 }
