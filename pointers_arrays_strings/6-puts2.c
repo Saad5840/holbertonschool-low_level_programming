911ba34aa759fab7637357c128ccebc1dea7614b
Saad <10704@holbertonstudents.com>
2025-02-26 15:40:50 +0000

    6-puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,12 @@
  */
 void puts2(char *str)
 {
-    int i;
+	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (i % 2 == 0)  /* Only print characters at even indices */
-            _putchar(str[i]);
-    }
-    _putchar('\n');  /* Ensure newline at the end */
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
 }
