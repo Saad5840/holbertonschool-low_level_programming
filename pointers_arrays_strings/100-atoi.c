92254e2e92d8b12a1617dd96cdc527fd48f1b8ea
Saad <10704@holbertonstudents.com>
2025-02-27 14:42:19 +0000

    100-atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,28 +6,27 @@
  *
  * Return: The integer value of the string.
  */
-
 int _atoi(char *s)
 {
+    int i = 0;
+    int sign = 1;
+    int num = 0;
+    int found_digit = 0;
 
-int i;
-int num = 0;
-int sign = 1;
+    while (s[i] != '\0')
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        else if (s[i] >= '0' && s[i] <= '9')
+        {
+            num = num * 10 + (s[i] - '0');
+            found_digit = 1;
+        }
+        else if (found_digit)
+            break;
 
+        i++;
+    }
 
-for (i = 0; s[i] != '\0'; i++)
-{
-	if (s[i] == '-')
-		sign = sign * -1;
-
-	if (s[i] >= '0' && s[i] <= '9')
-	{
-		num = num * 10 + (s[i] - '0');
-		if (s[i + 1] < '0' || s[i + 1] > '9')
-			break;
+    return (num * sign);
 }
-}
-return num * sign;
-}
-
-
