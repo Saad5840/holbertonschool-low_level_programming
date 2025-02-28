2d04c934f778e45774400200b01ebca13ac528ac
Saad <10704@holbertonstudents.com>
2025-02-28 13:27:46 +0000

    6-cap

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,26 +8,26 @@
  */
 char *cap_string(char *str)
 {
-    int i, j;
-    int capitalize = 1;
-    char separators[] = " \t\n,;.!?\"(){}";
+	int i, j;
+	int capitalize = 1;
+	char separators[] = " \t\n,;.!?\"(){}";
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (capitalize && str[i] >= 'a' && str[i] <= 'z')
-            str[i] -= 32;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (capitalize && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
 
-        capitalize = 0;
+		capitalize = 0;
 
-        for (j = 0; separators[j] != '\0'; j++)
-        {
-            if (str[i] == separators[j])
-            {
-                capitalize = 1;
-                break;
-            }
-        }
-    }
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (str[i] == separators[j])
+			{
+				capitalize = 1;
+				break;
+			}
+		}
+	}
 
-    return (str);
+	return (str);
 }
