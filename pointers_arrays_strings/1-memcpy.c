37092e3b4e8a335f8d6268c2423f0d5847023d18
Saad <10704@holbertonstudents.com>
2025-03-02 19:38:55 +0000

    1-memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies memory area
+ * @dest: Pointer to the destination memory where bytes are copied to
+ * @src: Pointer to the source memory where bytes are copied from
+ * @n: Number of bytes to copy
+ *
+ * Description: Copies `n` bytes from `src` to `dest`.
+ * Does not handle overlapping memory regions.
+ *
+ * Return: Pointer to `dest`
+ */
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+unsigned int i;
+
+
+for (i = 0; i < n; i++)
+{
+	src[i] =  dest[i];
+}
+
+return (dest);
+}
