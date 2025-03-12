6197d847149e850fd9b487ab9346d8ed88e83aba
saad <10704@holbertonstudents.com>
2025-03-12 14:12:11 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+
+
+
+/**
+ * struct dog - Structure to store dog information
+ * @name: Dog's name (string)
+ * @age: Dog's age (float)
+ * @owner: Dog's owner (string)
+ */
+
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+
+};
+
