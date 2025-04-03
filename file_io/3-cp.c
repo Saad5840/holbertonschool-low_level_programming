03d2ebdd1f250b0042533c31ddec44e41d44e277
Your Name <you@example.com>
2025-04-03 08:18:02 +0000

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,108 +1,59 @@
-#include "main.h"
 #include <fcntl.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+#include "holberton.h"
 
-#define BUFFER_SIZE 1024
 
-/**
- * error_exit - Prints an error message to stderr and exits.
- * @exit_code: The exit code to use.
- * @msg: The error message prefix.
- * @filename: The file associated with the error.
- */
-void error_exit(int exit_code, const char *msg, const char *filename)
-{
-	dprintf(STDERR_FILENO, "%s %s\n", msg, filename);
-	exit(exit_code);
-}
+#define READ_ERR "Error: Can't read from file %s\n"
+#define WRITE_ERR "Error: Can't write to %s\n"
 
 /**
- * close_fd - Closes a file descriptor and exits with code 100 if it fails.
- * @fd: The file descriptor to close.
- */
-void close_fd(int fd)
+ * main - copies the content of a file to another file
+ * @argc: number parameters
+ * @argv: argumentos
+ * Return: int -1 id fails
+ **/
+int main(int argc, char **argv)
 {
-	if (close(fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
+	int int_open, int_open2, on_close, r, w;
+	char *buff[1024];
 
-/**
- * main - Copies the content of a file to another file.
- * @ac: Argument count.
- * @av: Argument vector.
- *
- * Return: 0 on success, or exits with codes 97, 98, 99, or 100 on error.
- */
-int main(int ac, char **av)
-{
-	int fd_from, fd_to;
-	ssize_t r, w;
-	char buffer[BUFFER_SIZE];
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	if (ac != 3)
+	int_open = open(argv[1], O_RDONLY);
+
+	if (int_open == -1)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file", av[1]);
-
-	/* Perform an initial read to test for read errors */
-	r = read(fd_from, buffer, BUFFER_SIZE);
-	if (r == -1)
+	int_open2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (int_open2 == -1)
 	{
-		close_fd(fd_from);
-		error_exit(98, "Error: Can't read from file", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		exit(98);
 	}
-
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+	while (1)
 	{
-		close_fd(fd_from);
-		error_exit(99, "Error: Can't write to", av[2]);
-	}
-
-	/* If data was read initially, write it first */
-	if (r > 0)
-	{
-		w = write(fd_to, buffer, r);
-		if (w == -1 || w != r)
+		r = read(int_open, buff, 1024);
+		if (r == -1)
+			dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
+		if (r > 0)
 		{
-			close_fd(fd_from);
-			close_fd(fd_to);
-			error_exit(99, "Error: Can't write to", av[2]);
-		}
+			w = write(int_open2, buff, r);
+			if (w == -1)
+				dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
+		} else
+			break;
 	}
-
-	/* Copy the rest of the file if data was read */
-	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		w = write(fd_to, buffer, r);
-		if (w == -1 || w != r)
-		{
-			close_fd(fd_from);
-			close_fd(fd_to);
-			error_exit(99, "Error: Can't write to", av[2]);
-		}
-	}
-
-	/* Check for any read error in the loop */
-	if (r == -1)
-	{
-		close_fd(fd_from);
-		close_fd(fd_to);
-		error_exit(98, "Error: Can't read from file", av[1]);
-	}
-
-	close_fd(fd_from);
-	close_fd(fd_to);
-
+	on_close = close(int_open2);
+	if (on_close == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", int_open2), exit(100);
+	on_close = close(int_open);
+	if (on_close == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", int_open), exit(100);
 	return (0);
 }
