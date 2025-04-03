d341db0194b6b0ce717169e578312da287a0c000
Your Name <you@example.com>
2025-04-03 08:11:58 +0000

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -32,67 +32,6 @@ void close_fd(int fd)
 }
 
 /**
- * initial_copy - Performs an initial read from the source file and writes
- *                that data to the destination file.
- * @fd_from: Source file descriptor.
- * @fd_to: Destination file descriptor.
- * @file_from: Name of the source file.
- * @file_to: Name of the destination file.
- */
-void initial_copy(int fd_from, int fd_to, char *file_from, char *file_to)
-{
-	ssize_t r, w;
-	char buffer[BUFFER_SIZE];
-
-	r = read(fd_from, buffer, BUFFER_SIZE);
-	if (r == -1)
-	{
-		close_fd(fd_from);
-		error_exit(98, "Error: Can't read from file", file_from);
-	}
-	if (r > 0)
-	{
-		w = write(fd_to, buffer, r);
-		if (w == -1 || w != r)
-		{
-			close_fd(fd_from);
-			close_fd(fd_to);
-			error_exit(99, "Error: Can't write to", file_to);
-		}
-	}
-}
-
-/**
- * copy_loop - Copies the remainder of the source file to the destination.
- * @fd_from: Source file descriptor.
- * @fd_to: Destination file descriptor.
- * @file_from: Name of the source file.
- * @file_to: Name of the destination file.
- */
-void copy_loop(int fd_from, int fd_to, char *file_from, char *file_to)
-{
-	ssize_t r, w;
-	char buffer[BUFFER_SIZE];
-
-	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		w = write(fd_to, buffer, r);
-		if (w == -1 || w != r)
-		{
-			close_fd(fd_from);
-			close_fd(fd_to);
-			error_exit(99, "Error: Can't write to", file_to);
-		}
-	}
-	if (r == -1)
-	{
-		close_fd(fd_from);
-		close_fd(fd_to);
-		error_exit(98, "Error: Can't read from file", file_from);
-	}
-}
-
-/**
  * main - Copies the content of a file to another file.
  * @ac: Argument count.
  * @av: Argument vector.
@@ -102,23 +41,43 @@ void copy_loop(int fd_from, int fd_to, char *file_from, char *file_to)
 int main(int ac, char **av)
 {
 	int fd_from, fd_to;
+	ssize_t r, w;
+	char buffer[BUFFER_SIZE];
 
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 		error_exit(98, "Error: Can't read from file", av[1]);
+
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close_fd(fd_from);
 		error_exit(99, "Error: Can't write to", av[2]);
 	}
-	initial_copy(fd_from, fd_to, av[1], av[2]);
-	copy_loop(fd_from, fd_to, av[1], av[2]);
+
+	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		w = write(fd_to, buffer, r);
+		if (w == -1 || w != r)
+		{
+			close_fd(fd_from);
+			close_fd(fd_to);
+			error_exit(99, "Error: Can't write to", av[2]);
+		}
+	}
+	if (r == -1)
+	{
+		close_fd(fd_from);
+		close_fd(fd_to);
+		error_exit(98, "Error: Can't read from file", av[1]);
+	}
+
 	close_fd(fd_from);
 	close_fd(fd_to);
 	return (0);
