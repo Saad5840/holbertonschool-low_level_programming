3e77074ef698b25ce802be0f1e673478d905afea
Your Name <you@example.com>
2025-04-03 07:37:14 +0000

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,13 +8,13 @@
 
 /**
  * error_exit - Prints an error message to stderr and exits.
- * @exit_code: The exit code to use.
- * @msg: The error message prefix.
- * @filename: The file associated with the error.
+ * @exit_code: Exit code to use.
+ * @message: Message prefix to display.
+ * @filename: Name of the file associated with the error.
  */
-void error_exit(int exit_code, const char *msg, const char *filename)
+void error_exit(int exit_code, const char *message, const char *filename)
 {
-	dprintf(STDERR_FILENO, "%s %s\n", msg, filename);
+	dprintf(STDERR_FILENO, "%s %s\n", message, filename);
 	exit(exit_code);
 }
 
@@ -23,7 +23,7 @@ void error_exit(int exit_code, const char *msg, const char *filename)
  * @ac: Argument count.
  * @av: Argument vector.
  *
- * Return: 0 on success, or exits with codes 97, 98, 99, or 100 on error.
+ * Return: 0 on success, or exits with specific error codes on failure.
  */
 int main(int ac, char **av)
 {
@@ -37,10 +37,23 @@ int main(int ac, char **av)
 		exit(97);
 	}
 
+	/* Open the source file */
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 		error_exit(98, "Error: Can't read from file", av[1]);
 
+	/*
+	 * Perform an initial read to ensure we can read from the source.
+	 * If this read fails, we exit with code 98 before opening the destination.
+	 */
+	r = read(fd_from, buffer, BUFFER_SIZE);
+	if (r == -1)
+	{
+		close(fd_from);
+		error_exit(98, "Error: Can't read from file", av[1]);
+	}
+
+	/* Open the destination file */
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
@@ -48,6 +61,19 @@ int main(int ac, char **av)
 		error_exit(99, "Error: Can't write to", av[2]);
 	}
 
+	/* If we read any data from the first call, write it */
+	if (r > 0)
+	{
+		w = write(fd_to, buffer, r);
+		if (w == -1 || w != r)
+		{
+			close(fd_from);
+			close(fd_to);
+			error_exit(99, "Error: Can't write to", av[2]);
+		}
+	}
+
+	/* Continue reading and writing in a loop */
 	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		w = write(fd_to, buffer, r);
@@ -58,6 +84,8 @@ int main(int ac, char **av)
 			error_exit(99, "Error: Can't write to", av[2]);
 		}
 	}
+
+	/* Check if the last read failed */
 	if (r == -1)
 	{
 		close(fd_from);
